37476886a8d656c2dbd0f62c8be7a9660813b870
Rahul Ahire <ahirerahul21@gmail.com>
2020-02-14 04:03:54 +0530

    first commit for testing

---
+++ precache-manifest.1ed00d75f63aa1f4f03cf23a0f41212d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d6b18a14a59f73682c7e312029ceaa6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee0b6ff3d52fbbff5778",
+    "url": "/static/css/2.648ed26d.chunk.css"
+  },
+  {
+    "revision": "b3110174b4f88c6618d2",
+    "url": "/static/css/main.c276bb12.chunk.css"
+  },
+  {
+    "revision": "ee0b6ff3d52fbbff5778",
+    "url": "/static/js/2.ddffb06c.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.ddffb06c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b3110174b4f88c6618d2",
+    "url": "/static/js/main.9578dd4d.chunk.js"
+  },
+  {
+    "revision": "9fe3b08a914237729b4b",
+    "url": "/static/js/runtime-main.19b3b6f3.js"
+  },
+  {
+    "revision": "8df4d4386df5324ba4fe28117fadb23e",
+    "url": "/static/media/about-1.8df4d438.webp"
+  },
+  {
+    "revision": "633735e17e03c5978877dd4e852b3402",
+    "url": "/static/media/about-sec-2.633735e1.webm"
+  },
+  {
+    "revision": "c008b1662bae5f2f3c73dc6ee07cb45e",
+    "url": "/static/media/about-sec-3.c008b166.webm"
+  },
+  {
+    "revision": "11c4fae20b38207cf3b63031edd064e8",
+    "url": "/static/media/blank_user.11c4fae2.webp"
+  },
+  {
+    "revision": "cab85babb3744ef70c0b607bce05a6c2",
+    "url": "/static/media/img-1.cab85bab.webp"
+  },
+  {
+    "revision": "7987573ec06d7563128cbfbf50ba665b",
+    "url": "/static/media/img-2.7987573e.webp"
+  },
+  {
+    "revision": "a1805d8c7c6d02a5319e6962e87e7a6a",
+    "url": "/static/media/img-3.a1805d8c.webp"
+  },
+  {
+    "revision": "f53115bb91228857d245c50166b6e4a5",
+    "url": "/static/media/portrait-of-woman-photographing-with-smart-phone-248021.f53115bb.webp"
+  }
+]);
